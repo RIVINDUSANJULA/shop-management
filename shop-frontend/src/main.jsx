3724ca5a60e95e30233bf975c7696f30b45c2c61
Rivindu <97971763+RIVINDUSANJULA@users.noreply.github.com>
2025-09-12 21:20:44 +0530

    First Work Commit in Branch of Frontend. Did The Structure of Navbar Start Uplaod Stock Part.

--- shop-frontend/src/main.jsx
+++ shop-frontend/src/main.jsx
@@ -3,8 +3,19 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { BrowserRouter, Routes, Route } from "react-router";
+import Navbar from './files/Components/navbar.jsx'
+
+import Upload from './files/JS/Upload.jsx'
+
 createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Navbar />
+     <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/upload" element={<Upload />} />
+    </Routes>
+  </StrictMode>
+  </BrowserRouter>,
 )
